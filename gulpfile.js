4f4703bd383e8be6b375821482ca0db66fc5ec3b
mtsukasa0607 <m.tsukasa0607@gmail.com>
2020-09-12 12:38:49 +0000

    sassの環境構築

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+  .pipe(sass({ouotStyle: 'expanded'}))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
